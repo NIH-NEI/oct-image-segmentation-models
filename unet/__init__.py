ab127942723ca7a163aa24c4be2d2b70cd8c3612
Bruno Alvisio <bruno@bioteam.net>
2021-11-10 08:16:26 -0800

    Fix TCL_LIBRARY env variable conflict

--- unet/__init__.py
+++ unet/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 # Semantic Versioning 2.0.0: https://semver.org/
 # 1. MAJOR version when you make incompatible API changes;
 # 2. MINOR version when you add functionality in a backwards-compatible manner;
@@ -8,3 +10,8 @@ import matplotlib
 # There is a memory buildup in matplotlib. For now current solution is to use "Agg"
 # For reference: https://github.com/matplotlib/mplfinance/issues/386
 matplotlib.use("Agg")
+
+# Some systems have the TCL_LIBRARY environment set which causes version conflicts. Thus, we unset
+# it during initialization
+if os.environ.get("TCL_LIBRARY"):
+    del os.environ["TCL_LIBRARY"]
