e34b07a639ea96c22d49fea0be2d127e29b818d1
Bruno Alvisio <bruno@bioteam.net>
2022-09-22 20:49:57 -0500

    Formatting and flake8 of 'model/training_parameters.py'

--- unet/model/training_parameters.py
+++ unet/model/training_parameters.py
@@ -7,26 +7,33 @@ class TrainingParams:
     """
     Parameters for training a network:
 
-        aug_fn_args: tuple of two-tuples containing augmentation function and argument pairs
+        aug_fn_args: tuple of two-tuples containing augmentation function and \
+            argument pairs
         _________
 
         aug_mode: mode to use for augmentation
 
-        none: no augmentations -> will just use what is in the images and labels arrays as is
-        one: for each image, one augmentation will be picked from the list of possible augmentation functions
-            chosen based on probabilities in aug_probs.
-        all: for each image, all augmentations will be performed creating a new separate image for each
+        none: no augmentations -> will just use what is in the images and \
+        labels arrays as is
+        one: for each image, one augmentation will be picked from the list of \
+        possible augmentation functions chosen based on probabilities in \
+        aug_probs.
+        all: for each image, all augmentations will be performed creating a \
+        new separate image for each
 
-        note that for patch mode: augs are applied to the full size images before being broken into patches
+        note that for patch mode: augs are applied to the full size images \
+        before being broken into patches
         _________
 
-        aug_probs: probabilities used for selecting augmentations in 'one' mode. Should be values between 0 and 1
-        which add to 1.
+        aug_probs: probabilities used for selecting augmentations in 'one' \
+        mode. Should be values between 0 and 1 which add to 1.
         _________
 
-        aug_val: boolean used to apply the same augmentation policy to the validation dataset.
+        aug_val: boolean used to apply the same augmentation policy to the \
+        validation dataset.
         _________
     """
+
     def __init__(
         self,
         training_dataset_path: Path,
@@ -40,18 +47,18 @@ class TrainingParams:
         epochs,
         batch_size,
         aug_fn_args=((aug.no_aug, {}),),
-        aug_mode='none',
+        aug_mode="none",
         aug_probs=(),
         aug_fly=False,
         aug_val=True,
         shuffle=True,
         model_save_best=True,
-        model_save_monitor=('val_acc', 'max'),
+        model_save_monitor=("val_acc", "max"),
         normalise=True,
         use_gen=True,
         use_tensorboard=False,
         class_weight=None,
-        channels_last: bool=True,
+        channels_last: bool = True,
     ):
         self.training_dataset_path = training_dataset_path
         self.training_dataset_name = training_dataset_name
@@ -87,5 +94,8 @@ class TrainingParams:
         else:
             self.metric_name = self.metric
 
-        if self.model_save_monitor[0] == 'val_acc':
-            self.model_save_monitor = ['val_' + self.metric_name, model_save_monitor[1]]
+        if self.model_save_monitor[0] == "val_acc":
+            self.model_save_monitor = [
+                "val_" + self.metric_name,
+                model_save_monitor[1],
+            ]
