354624d3aa1142c4124a062dad7686eedafa384e
Bruno Alvisio <bruno@bioteam.net>
2022-09-22 20:40:15 -0500

    Formatting and flake8 of 'model/training.py'

--- unet/model/training.py
+++ unet/model/training.py
@@ -23,6 +23,7 @@ from unet.model import unet
 
 mlflow.autolog()
 
+
 def save_config_file(
     save_foldername: Path,
     model_name,
@@ -30,48 +31,63 @@ def save_config_file(
     train_params,
     train_imdb,
     val_imdb,
-    opt):
+    opt,
+):
     config_filename = save_foldername / Path("config.hdf5")
 
-    config_file = h5py.File(config_filename, 'w')
+    config_file = h5py.File(config_filename, "w")
 
-    config_file.attrs["timestamp"] = np.array(timestamp, dtype='S100')
-    config_file.attrs["model_name"] = np.array(model_name, dtype='S1000')
-    config_file.attrs["train_imdb"] = np.array(train_imdb.filename, dtype='S100')
-    config_file.attrs["val_imdb"] = np.array(val_imdb.filename, dtype='S100')
+    config_file.attrs["timestamp"] = np.array(timestamp, dtype="S100")
+    config_file.attrs["model_name"] = np.array(model_name, dtype="S1000")
+    config_file.attrs["train_imdb"] = np.array(
+        train_imdb.filename, dtype="S100"
+    )
+    config_file.attrs["val_imdb"] = np.array(val_imdb.filename, dtype="S100")
     config_file.attrs["epochs"] = train_params.epochs
-    config_file.attrs["dim_names"] = np.array(train_imdb.dim_names, dtype='S100')
-    config_file.attrs["type"] = np.array(train_imdb.type, dtype='S100')
-    if train_imdb.type == 'patch':
-        config_file.attrs["patch_size"] = np.array((train_imdb.image_width, train_imdb.image_height))
+    config_file.attrs["dim_names"] = np.array(
+        train_imdb.dim_names, dtype="S100"
+    )
+    config_file.attrs["type"] = np.array(train_imdb.type, dtype="S100")
+    if train_imdb.type == "patch":
+        config_file.attrs["patch_size"] = np.array(
+            (train_imdb.image_width, train_imdb.image_height)
+        )
 
     if train_imdb.dim_inds is None:
-        config_file.attrs["train_dim_inds"] = np.array("all", dtype='S100')
+        config_file.attrs["train_dim_inds"] = np.array("all", dtype="S100")
     else:
         dim_count = 0
         for dim_ind in train_imdb.dim_inds:
             if dim_ind is not None:
-                config_file.attrs["train_dim_ind: " + train_imdb.dim_names[dim_count]] = dim_ind
+                config_file.attrs[
+                    "train_dim_ind: " + train_imdb.dim_names[dim_count]
+                ] = dim_ind
             dim_count += 1
 
     if val_imdb.dim_inds is None:
-        config_file.attrs["val_dim_inds"] = np.array("all", dtype='S100')
+        config_file.attrs["val_dim_inds"] = np.array("all", dtype="S100")
     else:
         dim_count = 0
         for dim_ind in val_imdb.dim_inds:
             if dim_ind is not None:
-                config_file.attrs["val_dim_ind: " + val_imdb.dim_names[dim_count]] = dim_ind
+                config_file.attrs[
+                    "val_dim_ind: " + val_imdb.dim_names[dim_count]
+                ] = dim_ind
             dim_count += 1
 
-    config_file.attrs["metric"] = np.array(train_params.metric_name, dtype='S100')
-    config_file.attrs["loss"] = np.array(train_params.loss_name, dtype='S100')
+    config_file.attrs["metric"] = np.array(
+        train_params.metric_name, dtype="S100"
+    )
+    config_file.attrs["loss"] = np.array(train_params.loss_name, dtype="S100")
     config_file.attrs["batch_size"] = train_params.batch_size
     config_file.attrs["shuffle"] = train_params.shuffle
     if train_imdb.padding is not None:
         config_file.attrs["padding"] = np.array(train_imdb.padding)
 
-    config_file.attrs["aug_mode"] = np.array(train_params.aug_mode, dtype='S100')
-    if train_params.aug_mode != 'none':
+    config_file.attrs["aug_mode"] = np.array(
+        train_params.aug_mode, dtype="S100"
+    )
+    if train_params.aug_mode != "none":
         for aug_ind in range(len(train_params.aug_fn_args)):
             aug_fn = train_params.aug_fn_args[aug_ind][0]
             aug_arg = train_params.aug_fn_args[aug_ind][1]
@@ -79,34 +95,61 @@ def save_config_file(
             aug_desc = aug_fn(None, None, None, aug_arg, True)
 
             if type(aug_arg) is not dict:
-                config_file.attrs["aug_" + str(aug_ind + 1)] = np.array(aug_desc, dtype='S1000')
+                config_file.attrs["aug_" + str(aug_ind + 1)] = np.array(
+                    aug_desc, dtype="S1000"
+                )
             else:
-                config_file.attrs["aug_" + str(aug_ind + 1)] = np.array(aug_fn.__name__, dtype='S100')
+                config_file.attrs["aug_" + str(aug_ind + 1)] = np.array(
+                    aug_fn.__name__, dtype="S100"
+                )
 
                 for aug_param_key in aug_arg.keys():
                     val = aug_arg[aug_param_key]
                     if type(val) is int or type(val) is float:
-                        config_file.attrs["aug_" + str(aug_ind + 1) + "_param: " + aug_param_key] = np.array(val)
+                        config_file.attrs[
+                            "aug_"
+                            + str(aug_ind + 1)
+                            + "_param: "
+                            + aug_param_key
+                        ] = np.array(val)
                     elif type(val) is str:
-                        config_file.attrs["aug_" + str(aug_ind + 1) + "_param: " + aug_param_key] = np.array(val,
-                                                                                                             dtype='S100')
-                    elif type(val) is list and (type(val[0]) is int or type(val[0]) is str or type(val[0]) is float):
-                        config_file.attrs["aug_" + str(aug_ind + 1) + "_param: " + aug_param_key] = np.array(str(val), dtype='S100')
+                        config_file.attrs[
+                            "aug_"
+                            + str(aug_ind + 1)
+                            + "_param: "
+                            + aug_param_key
+                        ] = np.array(val, dtype="S100")
+                    elif type(val) is list and (
+                        type(val[0]) is int
+                        or type(val[0]) is str
+                        or type(val[0]) is float
+                    ):
+                        config_file.attrs[
+                            "aug_"
+                            + str(aug_ind + 1)
+                            + "_param: "
+                            + aug_param_key
+                        ] = np.array(str(val), dtype="S100")
 
-            if train_params.aug_mode == 'one':
-                config_file.attrs["aug_probs"] = \
-                    np.array(train_params.aug_probs)
+            if train_params.aug_mode == "one":
+                config_file.attrs["aug_probs"] = np.array(
+                    train_params.aug_probs
+                )
 
         config_file.attrs["aug_fly"] = train_params.aug_fly
         config_file.attrs["aug_val"] = train_params.aug_val
 
-    config_file.attrs["optimizer"] = np.array(train_params.opt_con.__name__, dtype='S100')
+    config_file.attrs["optimizer"] = np.array(
+        train_params.opt_con.__name__, dtype="S100"
+    )
 
     opt_config = opt.get_config()
 
     for key in opt_config:
         if type(opt_config[key]) is dict:
-            config_file.attrs["opt_param: " + key] = np.string_(str(opt_config[key]))
+            config_file.attrs["opt_param: " + key] = np.string_(
+                str(opt_config[key])
+            )
         else:
             config_file.attrs["opt_param: " + key] = opt_config[key]
 
@@ -115,7 +158,9 @@ def save_config_file(
     config_file.attrs["ram_load"] = train_imdb.ram_load
 
 
-def train_network(train_imdb, val_imdb, train_params, input_channels, num_classes):
+def train_network(
+    train_imdb, val_imdb, train_params, input_channels, num_classes
+):
     strategy = tf.distribute.MirroredStrategy()
     log.info(f"Number of devices: {strategy.num_replicas_in_sync}")
 
@@ -131,10 +176,15 @@ def train_network(train_imdb, val_imdb, train_params, input_channels, num_classe
     initial_model_path = train_params.initial_model
     if initial_model_path:
         log.info(f"Starting training from model: {initial_model_path}")
-        # TODO: Fix, 'descs' should be read from some metadata field or separate config
-        model, model_name, model_name_short = [utils.load_model(initial_model_path), "U-net", "U-net"]
+        # TODO: Fix, 'descs' should be read from some metadata field or
+        # separate config
+        model, model_name, model_name_short = [
+            utils.load_model(initial_model_path),
+            "U-net",
+            "U-net",
+        ]
     else:
-        log.info(f"Starting training from scratch oct-unet model")
+        log.info("Starting training from scratch oct-unet model")
         with strategy.scope():
             model, model_name, model_name_short = unet.get_standard_model(
                 input_channels=input_channels,
@@ -155,12 +205,12 @@ def train_network(train_imdb, val_imdb, train_params, input_channels, num_classe
     if use_gen is False and ram_load == 0:
         print("Incompatible parameter selection")
         exit(1)
-    elif ram_load == 0 and aug_fly is False and aug_mode != 'none':
+    elif ram_load == 0 and aug_fly is False and aug_mode != "none":
         print("Incompatible parameter selection")
         exit(1)
 
     if aug_val is False:
-        aug_val_mode = 'none'
+        aug_val_mode = "none"
         aug_val_fn_args = []
         aug_val_probs = []
         aug_val_fly = False
@@ -181,16 +231,34 @@ def train_network(train_imdb, val_imdb, train_params, input_channels, num_classe
     timestamp = utils.get_timestamp()
 
     results_location = train_params.results_location
-    save_foldername = results_location / Path(timestamp + "_" + model_name_short + "_" + dataset_name)
+    save_foldername = results_location / Path(
+        timestamp + "_" + model_name_short + "_" + dataset_name
+    )
 
     if not os.path.exists(save_foldername):
         os.makedirs(save_foldername)
     else:
         count = 2
-        testsave_foldername = results_location / Path(timestamp + "_" + str(count) + "_" + model_name_short + "_" + dataset_name)
+        testsave_foldername = results_location / Path(
+            timestamp
+            + "_"
+            + str(count)
+            + "_"
+            + model_name_short
+            + "_"
+            + dataset_name
+        )
         while os.path.exists(testsave_foldername):
             count += 1
-            testsave_foldername = results_location / Path(timestamp + "_" + str(count) + "_" + model_name_short + "_" + dataset_name)
+            testsave_foldername = results_location / Path(
+                timestamp
+                + "_"
+                + str(count)
+                + "_"
+                + model_name_short
+                + "_"
+                + dataset_name
+            )
 
         save_foldername = testsave_foldername
         os.makedirs(save_foldername)
@@ -201,14 +269,14 @@ def train_network(train_imdb, val_imdb, train_params, input_channels, num_classe
         filepath=save_foldername / Path(epoch_model_name),
         save_best_only=save_best,
         monitor=monitor[0],
-        mode=monitor[1]
+        mode=monitor[1],
     )
 
     history = training_callbacks.SaveEpochInfo(
         save_folder=save_foldername,
         model_name=model_name,
         train_params=train_params,
-        train_imdb=train_imdb
+        train_imdb=train_imdb,
     )
 
     if use_tensorboard is True:
@@ -218,13 +286,21 @@ def train_network(train_imdb, val_imdb, train_params, input_channels, num_classe
             write_graph=False,
             write_images=True,
             histogram_freq=1,
-            batch_size=batch_size
+            batch_size=batch_size,
         )
         callbacks_list = [savemodel, history, tensorboard]
     else:
         callbacks_list = [savemodel, history]
 
-    save_config_file(save_foldername, model_name, timestamp, train_params, train_imdb, val_imdb, optimizer)
+    save_config_file(
+        save_foldername,
+        model_name,
+        timestamp,
+        train_params,
+        train_imdb,
+        val_imdb,
+        optimizer,
+    )
 
     train_gen = data_gen.DataGenerator(
         train_imdb,
@@ -253,23 +329,28 @@ def train_network(train_imdb, val_imdb, train_params, input_channels, num_classe
     train_gen_total_samples = train_gen.get_total_samples()
     if batch_size > train_gen_total_samples:
         log.error(
-            f"The batch size ({batch_size}) cannot be larger than the number of training samples " \
-            f"({train_gen_total_samples})"
+            f"The batch size ({batch_size}) cannot be larger than the number "
+            f"of training samples ({train_gen_total_samples})"
         )
         exit(1)
-    log.info(f"Train generator total number of samples: {train_gen_total_samples}")
+    log.info(
+        f"Train generator total number of samples: {train_gen_total_samples}"
+    )
 
     val_gen_total_samples = val_gen.get_total_samples()
     if batch_size > val_gen_total_samples:
         log.error(
-            f"The batch size ({batch_size}) cannot be larger than the number of validation samples "\
-            f"({val_gen_total_samples})"
+            f"The batch size ({batch_size}) cannot be larger than the number "
+            f"of validation samples ({val_gen_total_samples})"
         )
         exit(1)
-    log.info(f"Validation generator total number of samples: {val_gen_total_samples}")
+    log.info(
+        "Validation generator total number of samples: "
+        f"{val_gen_total_samples}"
+    )
 
     model.summary()
-    model_info = model.fit(
+    model.fit(
         x=train_gen,
         validation_data=val_gen,
         epochs=epochs,
@@ -281,27 +362,33 @@ def train_network(train_imdb, val_imdb, train_params, input_channels, num_classe
 
 def train_model(
     training_params: tparams.TrainingParams,
-    mlflow_params: MLflowParameters=None,
+    mlflow_params: MLflowParameters = None,
 ):
     if mlflow_params:
         if mlflow_params.username:
             os.environ["MLFLOW_TRACKING_USERNAME"] = mlflow_params.username
         else:
-            if not os.getenv("MLFLOW_TRACKING_USERNAME"): # Only set env variable if not already set
+            # Only set env variable if not already set
+            if not os.getenv("MLFLOW_TRACKING_USERNAME"):
                 os.environ["MLFLOW_TRACKING_USERNAME"] = getpass.getuser()
         if mlflow_params.password:
             os.environ["MLFLOW_TRACKING_PASSWORD"] = mlflow_params.password
         else:
             if not os.environ.get("MLFLOW_TRACKING_PASSWORD"):
-                log.error("MLFlow password not provided and MLFLOW_TRACKING_PASSWORD environment "
-                "not set. Exiting...")
+                log.error(
+                    "MLFlow password not provided and "
+                    "MLFLOW_TRACKING_PASSWORD environment not set. Exiting..."
+                )
                 exit(1)
 
         mlflow.set_tracking_uri(mlflow_params.tracking_uri)
         try:
             mlflow.set_experiment(mlflow_params.experiment)
         except MlflowException as e:
-            log.exception(msg=f"An error occurred while setting MLflow experiment. Exception message: {e.message}")
+            log.exception(
+                msg=f"An error occurred while setting MLflow "
+                f"experiment. Exception message: {e.message}"
+            )
             sys.exit(1)
 
     if training_params.channels_last:
@@ -310,10 +397,15 @@ def train_model(
     training_dataset_path = training_params.training_dataset_path
     training_hdf5_file = h5py.File(training_dataset_path, "r")
 
-    # images numpy array should be of the shape: (number of images, image width, image height, 1)
-    # segments numpy array should be of the shape: (number of images, number of boundaries, image width)
-    train_images, train_labels = dataset_loader.load_training_data(training_hdf5_file)
-    val_images, val_labels = dataset_loader.load_validation_data(training_hdf5_file)
+    # images numpy array should be of the shape: (number of images, image
+    # width, image height, 1) segments numpy array should be of the shape:
+    # (number of images, number of boundaries, image width)
+    train_images, train_labels = dataset_loader.load_training_data(
+        training_hdf5_file
+    )
+    val_images, val_labels = dataset_loader.load_validation_data(
+        training_hdf5_file
+    )
 
     num_classes = len(np.unique(train_labels))
     log.info(f"Detected {num_classes} classes")
